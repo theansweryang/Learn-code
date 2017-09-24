c7837bea2682f561c28351ead204696909a96207
theansweryang <theansweryang@gmail.com>
2017-09-24 16:59:25 -0500

    Update python cursor demo.py

--- python cursor demo.py
+++ python cursor demo.py
@@ -28,27 +28,3 @@ ab=[107.8173305, -37.4966125]
 editcursor.insertRow([ab,'heating'])
 del editcursor#del很重要，del之后data才不会lock
 
-
--------------------------------------------------------------------------------------------------------------------------
--------------------------------------------------------------------------------------------------------------------------
--------------------------------------------------------------------------------------------------------------------------
-##only use da. class to search and update.. 
-#a_code_to_read_attribute_table
-fields=[ i.name for i in arcpy.ListFields('lc2011_LC')]#get all the fields,记住要加上name
-srCursor= arcpy.da.SearchCursor('lc2011_LC',fields)#建立一个SEARCH cursor
-for row in srCursor:
-    print (row) #each row is a tuple
-del srchcur 
-
-#a_code_to_update_attribute_table
-updateCursor= arcpy.da.UpdateCursor('lc2011_LC',fields)#建立一个SEARCH cursor
-for row in updateCursor:
-    print (row)#each row is a list
-del updateCursor 
-
-updateCursor= arcpy.da.UpdateCursor('lc2011_LC',fields)#建立一个SEARCH cursor
-for row in updateCursor:
-    row[-1]=row[0] #here you can make changes
-    updateCursor.updateRow(row)#don't forget to updateRow
-del updateCursor 
-
